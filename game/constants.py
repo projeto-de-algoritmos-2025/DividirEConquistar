1f1ef71d2cc68cf5130e8a1df1c2005d107b036e
Jlmsousa <jlmsousa@hotmail.com>
2025-06-26 22:48:09 -0300

    update constants

--- game/constants.py
+++ game/constants.py
@@ -7,10 +7,11 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
 NUM_ENEMIES = 10
-inversion_limit = 60
+inversion_limit = 40
 MAX_INV_TIME = 5
 MAX_REVEALS = 2
 REVEAL_DURATION = 1
+INITIAL_LIVES = 3
 
 ENEMY_COLORS = [
     (200, 50, 50),
